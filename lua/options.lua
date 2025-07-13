05c176c6f044e20c65424e480643e06813e7ca91
Alex <alex.podikoglou@gmail.com>
2025-07-13 16:29:18 +0300

    indent

--- lua/options.lua
+++ lua/options.lua
@@ -5,6 +5,8 @@ vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 
+vim.opt.smartindent = false
+vim.opt.autoindent = false
 vim.opt.guicursor = "n-v-i-c:block-Cursor"
 
 -- color scheme
