5c499aff5c6d2cc264a3c8feacc814adba56114b
Alex <alex.podikoglou@gmail.com>
2025-09-18 16:03:24 +0300

    format

--- lua/options.lua
+++ lua/options.lua
@@ -67,23 +67,23 @@ vim.opt.clipboard = "unnamedplus"
 local theme_script_path = vim.fn.expand("~/.local/share/tinted-theming/tinty/base16-vim-colors-file.vim")
 
 local function file_exists(file_path)
-	return vim.fn.filereadable(file_path) == 1 and true or false
+  return vim.fn.filereadable(file_path) == 1 and true or false
 end
 
 local function handle_focus_gained()
-	if file_exists(theme_script_path) then
-		vim.cmd("source " .. theme_script_path)
-	end
+  if file_exists(theme_script_path) then
+    vim.cmd("source " .. theme_script_path)
+  end
 end
 
 if file_exists(theme_script_path) then
-	vim.o.termguicolors = true
-	vim.g.tinted_colorspace = 256
-	vim.g.tinted_background_transparent = "1"
+  vim.o.termguicolors = true
+  vim.g.tinted_colorspace = 256
+  vim.g.tinted_background_transparent = "1"
 
-	vim.cmd("source " .. theme_script_path)
+  vim.cmd("source " .. theme_script_path)
 
-	vim.api.nvim_create_autocmd("FocusGained", {
-		callback = handle_focus_gained,
-	})
+  vim.api.nvim_create_autocmd("FocusGained", {
+    callback = handle_focus_gained,
+  })
 end
