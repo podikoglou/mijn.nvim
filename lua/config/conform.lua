867d9fa363b67c53eea6cce8a95368fd6b820e65
Alex <alex.podikoglou@gmail.com>
2025-10-04 12:54:55 +0300

    run biome-check and biome-organize-imports along with biome

--- lua/config/conform.lua
+++ lua/config/conform.lua
@@ -3,8 +3,8 @@ require("conform").setup({
 		lua = { "stylua" },
 		python = { "ruff" },
 		rust = { "rustfmt", lsp_format = "fallback" },
-		javascript = { "biome" },
-		typescript = { "biome" },
+		javascript = { "biome", "biome-check", "biome-organize-imports" },
+		typescript = { "biome", "biome-check", "biome-organize-imports" },
 	},
 	format_on_save = {
 		timeout_ms = 500,
