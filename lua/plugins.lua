19493d787b25a7d9e8d0623876227bff6d2b631b
Alex <alex.podikoglou@gmail.com>
2025-10-07 03:04:39 +0300

    FINE. format.

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,33 +1,33 @@
 vim.pack.add({
-	-- icons
-	"https://github.com/echasnovski/mini.icons",
-	"https://github.com/lewis6991/gitsigns.nvim",
-	"https://github.com/nvim-tree/nvim-web-devicons",
+  -- icons
+  "https://github.com/echasnovski/mini.icons",
+  "https://github.com/lewis6991/gitsigns.nvim",
+  "https://github.com/nvim-tree/nvim-web-devicons",
 
-	-- ui
-	"https://github.com/romgrk/barbar.nvim",
-	"https://github.com/hiphish/rainbow-delimiters.nvim",
-	"https://github.com/nvim-tree/nvim-tree.lua",
-	"https://github.com/nvim-mini/mini.pick",
+  -- ui
+  "https://github.com/romgrk/barbar.nvim",
+  "https://github.com/hiphish/rainbow-delimiters.nvim",
+  "https://github.com/nvim-tree/nvim-tree.lua",
+  "https://github.com/nvim-mini/mini.pick",
 
-	-- editing
-	"https://github.com/nvim-mini/mini.comment",
-	"https://github.com/stevearc/conform.nvim",
-	"https://github.com/rafamadriz/friendly-snippets",
-	"https://github.com/nvim-mini/mini.snippets",
-	"https://github.com/smoka7/hop.nvim",
-	"https://github.com/nvim-mini/mini.pairs",
+  -- editing
+  "https://github.com/nvim-mini/mini.comment",
+  "https://github.com/stevearc/conform.nvim",
+  "https://github.com/rafamadriz/friendly-snippets",
+  "https://github.com/nvim-mini/mini.snippets",
+  "https://github.com/smoka7/hop.nvim",
+  "https://github.com/nvim-mini/mini.pairs",
 
-	-- lsp & language support
-	"https://github.com/williamboman/mason.nvim",
-	"https://github.com/neovim/nvim-lspconfig",
-	"https://github.com/nvim-treesitter/nvim-treesitter",
+  -- lsp & language support
+  "https://github.com/williamboman/mason.nvim",
+  "https://github.com/neovim/nvim-lspconfig",
+  "https://github.com/nvim-treesitter/nvim-treesitter",
 
-	-- completion
-	"https://github.com/nvim-mini/mini.completion",
+  -- completion
+  "https://github.com/nvim-mini/mini.completion",
 
-	-- languages
-	"https://github.com/Julian/lean.nvim",
+  -- languages
+  "https://github.com/Julian/lean.nvim",
 })
 
 require("config.mini_icons")
@@ -37,6 +37,7 @@ require("config.hop")
 require("config.mini_comment")
 require("config.conform")
 require("config.mini_pick")
+
 require("config.nvim-tree")
 require("config.mason")
 require("config.mini_completion")
