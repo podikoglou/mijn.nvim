8486a1a303545efa6a6fbf8cf0d78cc6fb479387
alex <alex.podikoglou@gmail.com>
2026-03-02 09:45:03 +0200

    omfg format

---
+++ lua/config/obsidian.lua
@@ -0,0 +1,10 @@
+require("obsidian").setup({
+  legacy_commands = false,
+  workspaces = {
+    {
+      name = "mijn",
+      path = "~/code/mijn"
+    }
+  }
+})
+vim.opt_local.conceallevel = 1
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,47 +1,48 @@
 vim.pack.add({
-	-- lib
-	"https://github.com/nvim-lua/plenary.nvim",
+  -- lib
+  "https://github.com/nvim-lua/plenary.nvim",
 
-	-- icons
-	"https://github.com/echasnovski/mini.icons",
-	"https://github.com/lewis6991/gitsigns.nvim",
-	"https://github.com/nvim-tree/nvim-web-devicons",
+  -- icons
+  "https://github.com/echasnovski/mini.icons",
+  "https://github.com/lewis6991/gitsigns.nvim",
+  "https://github.com/nvim-tree/nvim-web-devicons",
 
-	-- ui
-	"https://github.com/hiphish/rainbow-delimiters.nvim",
-	"https://github.com/nvim-tree/nvim-tree.lua",
-	"https://github.com/nvim-telescope/telescope.nvim",
-	"https://github.com/kepano/flexoki-neovim",
-	-- future: https://nvim-mini.org/mini.nvim/readmes/mini-hues.html
+  -- ui
+  "https://github.com/hiphish/rainbow-delimiters.nvim",
+  "https://github.com/nvim-tree/nvim-tree.lua",
+  "https://github.com/nvim-telescope/telescope.nvim",
+  "https://github.com/kepano/flexoki-neovim",
   "https://github.com/moll/vim-bbye",
+  -- future: https://nvim-mini.org/mini.nvim/readmes/mini-hues.html
 
-	-- editing
-	"https://github.com/nvim-mini/mini.comment",
-	"https://github.com/stevearc/conform.nvim",
-	"https://github.com/rafamadriz/friendly-snippets",
-	"https://github.com/nvim-mini/mini.snippets",
-	"https://github.com/smoka7/hop.nvim",
-	"https://github.com/nvim-mini/mini.pairs",
-	"https://github.com/nvim-mini/mini.keymap",
-	"https://github.com/nvim-mini/mini.basics",
-	"https://github.com/nvim-mini/mini.statusline",
-	"https://github.com/nvim-mini/mini.tabline",
-	"https://github.com/nvim-mini/mini.indentscope",
-	"https://github.com/monaqa/dial.nvim",
-	"https://github.com/kevinhwang91/promise-async",
-	"https://github.com/kevinhwang91/nvim-ufo",
+  -- editing
+  "https://github.com/nvim-mini/mini.comment",
+  "https://github.com/stevearc/conform.nvim",
+  "https://github.com/rafamadriz/friendly-snippets",
+  "https://github.com/nvim-mini/mini.snippets",
+  "https://github.com/smoka7/hop.nvim",
+  "https://github.com/nvim-mini/mini.pairs",
+  "https://github.com/nvim-mini/mini.keymap",
+  "https://github.com/nvim-mini/mini.basics",
+  "https://github.com/nvim-mini/mini.statusline",
+  "https://github.com/nvim-mini/mini.tabline",
+  "https://github.com/nvim-mini/mini.indentscope",
+  "https://github.com/monaqa/dial.nvim",
+  "https://github.com/kevinhwang91/promise-async",
+  "https://github.com/kevinhwang91/nvim-ufo",
 
-	-- lsp & language support
-	"https://github.com/williamboman/mason.nvim",
-	"https://github.com/neovim/nvim-lspconfig",
-	"https://github.com/nvim-treesitter/nvim-treesitter",
+  -- lsp & language support
+  "https://github.com/williamboman/mason.nvim",
+  "https://github.com/neovim/nvim-lspconfig",
+  "https://github.com/nvim-treesitter/nvim-treesitter",
+  "https://github.com/obsidian-nvim/obsidian.nvim",
 
-	-- completion
-	"https://github.com/nvim-mini/mini.completion",
+  -- completion
+  "https://github.com/nvim-mini/mini.completion",
 
-	-- languages
-	"https://github.com/Julian/lean.nvim",
-	"https://github.com/mrcjkb/rustaceanvim",
+  -- languages
+  "https://github.com/Julian/lean.nvim",
+  "https://github.com/mrcjkb/rustaceanvim",
 })
 
 require("config.dial")
@@ -63,3 +64,4 @@ require("config.mini_snippets")
 require("config.treesitter")
 require("config.lean4")
 require("config.ufo")
+require("config.obsidian")
