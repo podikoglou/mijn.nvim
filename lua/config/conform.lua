50bd6fe1c51172d4d3c6cc2f5f028f779bc17fb9
alex <alex.podikoglou@gmail.com>
2026-03-21 15:29:28 +0200

    oxfmt

--- lua/config/conform.lua
+++ lua/config/conform.lua
@@ -1,13 +1,17 @@
 require("conform").setup({
-	formatters_by_ft = {
-		lua = { "stylua" },
-		python = { "ruff" },
-		rust = { "rustfmt", lsp_format = "fallback" },
-		javascript = { "biome", "biome-check", "biome-organize-imports" },
-		typescript = { "biome", "biome-check", "biome-organize-imports" },
-	},
-	format_on_save = {
-		timeout_ms = 500,
-		lsp_format = "fallback",
-	},
+  formatters_by_ft = {
+    lua = { "stylua" },
+    python = { "ruff" },
+    rust = { "rustfmt", lsp_format = "fallback" },
+    typescriptreact = { "oxfmt" },
+    typescript = { "oxfmt" },
+    javascript = { "oxfmt" },
+    javascriptreact = { "oxfmt" },
+    json = { "oxfmt" },
+    vue = { "oxfmt" },
+  },
+  format_on_save = {
+    timeout_ms = 500,
+    lsp_format = "fallback",
+  },
 })
