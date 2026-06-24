79248c258ad13c9eae6e3c240e999b5e36435dce
alex <alex.podikoglou@gmail.com>
2026-06-24 19:50:43 +0300

    remove blanket lint autocmd that was calling uninstalled vale

--- lua/config/nvim_lint.lua
+++ lua/config/nvim_lint.lua
@@ -5,13 +5,4 @@ vim.filetype.add({
   },
 })
 
-require('lint').linters_by_ft = {
-  markdown = { 'vale' },
-  ["yaml.openapi"] = { "vacuum" }
-}
 
-vim.api.nvim_create_autocmd({ "BufWritePost" }, {
-  callback = function()
-    require("lint").try_lint()
-  end,
-})
