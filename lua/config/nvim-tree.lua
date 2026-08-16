504a5f303465951fa7cd94a31e410c7aeb6055b4
alex <alex.podikoglou@gmail.com>
2026-08-16 18:52:28 +0300

    refactor(tree): remove local transparency overrides

--- lua/config/nvim-tree.lua
+++ lua/config/nvim-tree.lua
@@ -4,14 +4,3 @@ vim.g.loaded_netrwPlugin = 1
 vim.opt.termguicolors = true
 
 require("nvim-tree").setup({})
-
--- Use the terminal background in the file tree.
-for _, group in ipairs({
-	"NvimTreeNormal",
-	"NvimTreeNormalNC",
-	"NvimTreeEndOfBuffer",
-	"NvimTreeVertSplit",
-	"NvimTreeWinSeparator",
-}) do
-	vim.api.nvim_set_hl(0, group, { bg = "NONE" })
-end
