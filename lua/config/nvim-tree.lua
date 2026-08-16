54c3fef1bfc6c1a99be639d27cc12430a4985e05
alex <alex.podikoglou@gmail.com>
2026-08-16 18:51:53 +0300

    fix(tree): make file tree transparent

--- lua/config/nvim-tree.lua
+++ lua/config/nvim-tree.lua
@@ -4,3 +4,14 @@ vim.g.loaded_netrwPlugin = 1
 vim.opt.termguicolors = true
 
 require("nvim-tree").setup({})
+
+-- Use the terminal background in the file tree.
+for _, group in ipairs({
+	"NvimTreeNormal",
+	"NvimTreeNormalNC",
+	"NvimTreeEndOfBuffer",
+	"NvimTreeVertSplit",
+	"NvimTreeWinSeparator",
+}) do
+	vim.api.nvim_set_hl(0, group, { bg = "NONE" })
+end
